c1b73542cb2a498e18745fc39bc717913bf6943e
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-08-22 22:34:53 +0000

    Added Paginator.js
    darcs-hash:20070822223453-1d602-d075bebd23577e1627386edd121677c1a8424d02.gz

---
+++ media/js/Paginator.js
@@ -0,0 +1,15 @@
+var Paginator =
+{
+    jumpToPage: function(pages)
+    {
+        var page = prompt("Enter a number between 1 and " + pages + " to jump to that page", "");
+        if (page != undefined)
+        {
+            page = parseInt(page, 10)
+            if (!isNaN(page) && page > 0 && page <= pages)
+            {
+                window.location.href = "?page=" + page;
+            }
+        }
+    }
+};
