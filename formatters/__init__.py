ab3c6f271d18ac6caa8423ffd91ca0c1bb4cf048
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-09-05 14:53:37 +0000

    Now performing basic validation of what's required from a post formatting module
    darcs-hash:20070905145337-1d602-cef3169406c0f3634ecac17fa9da66e040d79456.gz

--- formatters/__init__.py
+++ formatters/__init__.py
@@ -1,8 +1,23 @@
 from forum import app_settings
 
+# Two-tuples of (attribute name, must be callable), defining the minimum
+# required of a post formatting module.
+POST_FORMATTING_MODULE_REQUIREMENTS = (
+    ('QUICK_HELP_TEMPLATE', False),
+    ('FULL_HELP_TEMPLATE', False),
+    ('format_post_body', True),
+    ('quote_post', True),
+)
+
 def get_post_formatter():
     try:
-        return __import__(app_settings.POST_FORMATTING_MODULE, {}, {}, [''])
+        mod = __import__(app_settings.POST_FORMATTING_MODULE, {}, {}, [''])
+        for attr, must_be_callable in POST_FORMATTING_MODULE_REQUIREMENTS:
+            if not hasattr(mod, attr) or \
+               must_be_callable and not callable(getattr(mod, attr)):
+                raise ValueError('The "%s" module does not define a %s"%s" attribute, which is required for it to be used as a post formatter.' % (
+                    app_settings.POST_FORMATTING_MODULE, must_be_callable and 'callable ' or '', attr))
+        return mod
     except ImportError, e:
         raise EnvironmentError('Could not import post formatting module "%s" (Is it on sys.path? Does it have syntax errors?): %s' % (app_settings.POST_FORMATTING_MODULE, e))
 
