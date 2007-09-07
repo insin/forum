2eecf80ffcfa2d1b9734d6d418c6dab634510e5d
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-09-07 00:12:51 +0000

    Fixed DOM whitespace issue in IE
    darcs-hash:20070907001251-1d602-990c33fe836d1f54ca393260ae88efb2db60310b.gz

--- media/js/Topic.js
+++ media/js/Topic.js
@@ -3,17 +3,20 @@ jQuery(function()
 {
     if (jQuery("#topic-actions-bottom").size() > 0)
     {
-        jQuery('<a href="#fast-reply">Fast Reply</a>').click(function()
+        jQuery("#topic-actions-bottom")
+        .prepend(document.createTextNode(" "))
+        .prepend(jQuery('<a href="#fast-reply">Fast Reply</a>').click(function()
         {
             jQuery("#fast-reply").toggle();
-        }).prependTo("#topic-actions-bottom");
+        }));
     }
 
     if (jQuery("#fast-reply-buttons").size() > 0)
     {
-        jQuery('<input type="button" value="Close Fast Reply">').click(function()
+        jQuery("#fast-reply-buttons")
+        .append(jQuery('<input type="button" value="Close Fast Reply">').click(function()
         {
             jQuery("#fast-reply").toggle();
-        }).appendTo("#fast-reply-buttons");
+        }));
     }
 });
