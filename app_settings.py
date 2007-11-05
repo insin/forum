7541a641524e9d4fe79805cecc6bbfc3d8bfbb00
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2007-11-05 22:42:09 +0000

    App settings are now expressed much more succinctly - thanks to Sandra24 and dougn in #django
    darcs-hash:20071105224209-1d602-fed350b928d0afb07e2e0fc1e75f36741d70de40.gz

--- app_settings.py
+++ app_settings.py
@@ -5,42 +5,11 @@ the appropriate settings.
 """
 from django.conf import settings
 
-try:
-    STANDALONE = settings.FORUM_STANDALONE
-except AttributeError:
-    STANDALONE = False
-
-try:
-    DEFAULT_POSTS_PER_PAGE = settings.FORUM_DEFAULT_POSTS_PER_PAGE
-except AttributeError:
-    DEFAULT_POSTS_PER_PAGE = 20
-
-try:
-    DEFAULT_TOPICS_PER_PAGE = settings.FORUM_DEFAULT_TOPICS_PER_PAGE
-except AttributeError:
-    DEFAULT_TOPICS_PER_PAGE = 30
-
-try:
-    POST_FORMATTING_MODULE = settings.FORUM_POST_FORMATTING_MODULE
-except AttributeError:
-    POST_FORMATTING_MODULE = 'forum.formatters.basic_formatter'
-
-try:
-    MAX_AVATAR_FILESIZE = settings.FORUM_MAX_AVATAR_FILESIZE
-except AttributeError:
-    MAX_AVATAR_FILESIZE = 512 * 1024 # 512 kB
-
-try:
-    ALLOWED_AVATAR_FORMATS = settings.FORUM_ALLOWED_AVATAR_FORMATS
-except AttributeError:
-    ALLOWED_AVATAR_FORMATS = ('GIF', 'JPEG', 'PNG')
-
-try:
-    MAX_AVATAR_DIMENSIONS = settings.FORUM_MAX_AVATAR_DIMENSIONS
-except AttributeError:
-    MAX_AVATAR_DIMENSIONS = (64, 64)
-
-try:
-    FORCE_AVATAR_DIMENSIONS = settings.FORUM_FORCE_AVATAR_DIMENSIONS
-except AttributeError:
-    FORCE_AVATAR_DIMENSIONS = True
+STANDALONE              = getattr(settings, 'FORUM_STANDALONE',              False)
+DEFAULT_POSTS_PER_PAGE  = getattr(settings, 'FORUM_DEFAULT_POSTS_PER_PAGE',  20)
+DEFAULT_TOPICS_PER_PAGE = getattr(settings, 'FORUM_DEFAULT_TOPICS_PER_PAGE', 30)
+POST_FORMATTING_MODULE  = getattr(settings, 'FORUM_POST_FORMATTING_MODULE',  'forum.formatters.basic_formatter')
+MAX_AVATAR_FILESIZE     = getattr(settings, 'FORUM_MAX_AVATAR_FILESIZE',     512 * 1024)
+ALLOWED_AVATAR_FORMATS  = getattr(settings, 'FORUM_ALLOWED_AVATAR_FORMATS',  ('GIF', 'JPEG', 'PNG'))
+MAX_AVATAR_DIMENSIONS   = getattr(settings, 'FORUM_MAX_AVATAR_DIMENSIONS',   (64, 64))
+FORCE_AVATAR_DIMENSIONS = getattr(settings, 'FORUM_FORCE_AVATAR_DIMENSIONS', True)
