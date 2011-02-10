7c76778551b56038f95809fdb7df9729b7fdf216
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-02-10 07:48:56 +0000

    Tidied up session backend

--- forum/sessions/redis_session_backend.py
+++ forum/sessions/redis_session_backend.py
@@ -1,28 +1,22 @@
-"""
-Redis session store by Adam Gomaa
-
-http://hg.gomaa.us/agdj/file/tip/agdj/lib/redis_session_backend.py
-"""
-from cPickle import loads, dumps
-
 from django.contrib.sessions.backends.base import SessionBase, CreateError
-from django.conf import settings
 
 from forum.redis_connection import r
 
 class SessionStore(SessionBase):
     """
-    A redis-based session store.
+    Implements a Redis session store.
     """
-    def __init__(self, session_key=None):
-        super(SessionStore, self).__init__(session_key)
-
     def load(self):
-        session_data = r.get(self.session_key)
+        session_data = r.get('session:%s' % self.session_key)
         if session_data is not None:
-            return loads(session_data)
+            return self.decode(session_data)
         self.create()
         return {}
+
+    def exists(self, session_key):
+        if r.exists('session:%s' % session_key):
+            return True
+        return False
 
     def create(self):
         while True:
@@ -30,30 +24,20 @@ class SessionStore(SessionBase):
             try:
                 self.save(must_create=True)
             except CreateError:
-                # Would be raised if the key wasn't unique
                 continue
             self.modified = True
             return
 
     def save(self, must_create=False):
-        if must_create:
-            # preserve=True -> SETNX
-            result = r.set(
-                self.session_key, dumps(self._get_session(no_load=must_create)), preserve=True)
-            if result == 0: # 0 == not created, 1 == created.
-                raise CreateError
-        else:
-            r.set(self.session_key, dumps(self._get_session(no_load=must_create)),)
-
-    def exists(self, session_key):
-        if r.exists(session_key):
-            return True
-        return False
+        key = 'session:%s' % self.session_key
+        if must_create and r.exists(key):
+            raise CreateError
+        r.set(key, self.encode(self._get_session(no_load=must_create)))
+        r.expire(key, self.get_expiry_age())
 
     def delete(self, session_key=None):
         if session_key is None:
             if self._session_key is None:
                 return
             session_key = self._session_key
-        r.delete(session_key)
-
+        r.delete('session:%s' % session_key)
