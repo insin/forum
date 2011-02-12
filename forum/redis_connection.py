8a26ee4809183c191fe0a86182f34d8e316aa0b4
Jonathan Buchanan <jonathan.buchanan@gmail.com>
2011-02-12 14:24:22 +0000

    Added constants for Redis keys and shortened the keys

--- forum/redis_connection.py
+++ forum/redis_connection.py
@@ -20,13 +20,20 @@ def close_connection(**kwargs):
     r.connection.disconnect()
 signals.request_finished.connect(close_connection)
 
+TOPIC_ViEWS = 't:%s:v'
+TOPIC_TRACKER = 'u:%s:t:%s'
+ACTIVE_USERS = 'au'
+USER_USERNAME = 'u:%s:un'
+USER_LAST_SEEN = 'u:%s:s'
+USER_DOING = 'u:%s:d'
+
 def increment_view_count(topic):
     """Increments the view count for a Topic."""
-    r.incr('topic:%s:views' % topic.pk)
+    r.incr(TOPIC_ViEWS % topic.pk)
 
 def get_view_counts(topic_ids):
     """Yields viewcounts for the given Topics."""
-    for view_count in r.mget(['topic:%s:views' % id for id in topic_ids]):
+    for view_count in r.mget([TOPIC_ViEWS % id for id in topic_ids]):
         if view_count:
             yield int(view_count)
         else:
@@ -37,7 +44,7 @@ def update_last_read_time(user, topic):
     Sets the last read time for a User in the given Topic, expiring in a
     fortnight.
     """
-    key = 'user:%s:topictracker:%s' % (user.pk, topic.pk)
+    key = TOPIC_TRACKER % (user.pk, topic.pk)
     last_read = datetime.datetime.now()
     expire_at = last_read + datetime.timedelta(days=14)
     r.set(key, int(time.mktime(last_read.timetuple())))
@@ -45,14 +52,14 @@ def update_last_read_time(user, topic):
 
 def get_last_read_time(user, topic_id):
     """Gets the last read time for a User in the given Topic."""
-    last_read = r.get('user:%s:topictracker:%s' % (user.pk, topic_id))
+    last_read = r.get(TOPIC_TRACKER % (user.pk, topic_id))
     if last_read:
         return datetime.datetime.fromtimestamp(int(last_read))
     return None
 
 def get_last_read_times(user, topics):
     """Gets last read times for a User in the given Topics."""
-    for last_read in r.mget(['user:%s:topictracker:%s' % (user.pk, t.pk)
+    for last_read in r.mget([TOPIC_TRACKER % (user.pk, t.pk)
                              for t in topics]):
         if last_read:
             yield datetime.datetime.fromtimestamp(int(last_read))
@@ -65,26 +72,26 @@ def seen_user(user, doing, item=None):
     their last seen time in the active users sorted set.
     """
     last_seen = int(time.mktime(datetime.datetime.now().timetuple()))
-    r.zadd('activeusers', user.pk, last_seen)
-    r.setnx('user:%s:username' % user.pk, user.username)
-    r.set('user:%s:lastseen' % user.pk, last_seen)
+    r.zadd(ACTIVE_USERS, user.pk, last_seen)
+    r.setnx(USER_USERNAME % user.pk, user.username)
+    r.set(USER_LAST_SEEN % user.pk, last_seen)
     if item:
         doing = '%s <a href="%s">%s</a>' % (
             doing, item.get_absolute_url(), escape(str(item)))
-    r.set('user:%s:doing' % user.pk, doing)
+    r.set(USER_DOING % user.pk, doing)
 
 def get_active_users(minutes_ago=30):
     """
     Yields active Users in the last ``minutes_ago`` minutes, returning
-    2-tuples of (user_detail_dict, last_seen_time) in low-to-high order
-    by time.
+    2-tuples of (user_detail_dict, last_seen_time) in most-to-least recent
+    order by time.
     """
     since = datetime.datetime.now() - datetime.timedelta(minutes=minutes_ago)
     since_time = int(time.mktime(since.timetuple()))
-    for user_id, last_seen in reversed(r.zrangebyscore('activeusers', since_time,
+    for user_id, last_seen in reversed(r.zrangebyscore(ACTIVE_USERS, since_time,
                                                        'inf', withscores=True)):
         yield (
-            {'id': int(user_id), 'username': r.get('user:%s:username' % user_id)},
+            {'id': int(user_id), 'username': r.get(USER_USERNAME % user_id)},
             datetime.datetime.fromtimestamp(int(last_seen)),
         )
 
@@ -93,10 +100,10 @@ def get_last_seen(user):
     Returns a 2-tuple of (last_seen, doing), where doing may contain HTML
     linking to the relevant place.
     """
-    last_seen = r.get('user:%s:lastseen' % user.pk)
+    last_seen = r.get(USER_LAST_SEEN % user.pk)
     if last_seen:
         last_seen = datetime.datetime.fromtimestamp(int(last_seen))
     else:
         last_seen = user.date_joined
-    doing = r.get('user:%s:doing' % user.pk)
+    doing = r.get(USER_DOING % user.pk)
     return last_seen, doing
